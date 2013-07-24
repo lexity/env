7f9cd2b446ff9aa5cdb132f02da8125c7bd42d6d
Lexity Vagrant <ops@lexity.com>
2013-07-24 21:54:14 +0000

    Added HOME paths

--- linux/vagrant/.custom.sh
+++ linux/vagrant/.custom.sh
@@ -1,1 +1,7 @@
 alias e="emacs"
+
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
+export CATALINA_HOME=/usr/share/tomcat7
+
+export GRAILS_HOME=/usr/src/grails-1.3.7
+PATH="$GRAILS_HOME/bin:$PATH"
